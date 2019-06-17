30a462e3fd0c87490c3a7fc094a94e307fcf6758
Amit Dhakad <amit0502dhakad@gmail.com>
2019-06-17 21:16:11 +0530

    added side drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { Toolbar } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SideDrawer from './SideDrawer';
 class Header extends Component {
+  state = {
+    drawerOpen: false
+  };
+
+  toogleDrawer = value => {
+    this.setState({ drawerOpen: value });
+  };
   render() {
     return (
       <AppBar
@@ -17,6 +27,18 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toogleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toogleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
